78c84df8fe61ec3365327c1ce32e7a009cbf8e31
Tracy Jiang <tracyjiangmx@gmail.com>
2021-05-08 16:16:00 -0700

    complete prob 1

---
+++ two_sum.py
@@ -0,0 +1,23 @@
+'''
+- create a dictionary to store numbers already looked through 
+#and its corresponding index
+- loop over the array
+- at each iteration, first calculate the number needed to add up to target
+- if that number already exist in the dictionary, return both indices
+- otherwise save the current element and index to dictionary
+'''
+
+
+def twoSum(nums, target):
+        
+    numsAppeared = {}
+        
+    for i in range(0, len(nums)):
+        num = nums[i]
+        targetNum = target - num
+        
+        if numsAppeared.get(targetNum) is not None:
+            return [i, numsAppeared[targetNum]]
+        else:
+            numsAppeared[num] = i
+
